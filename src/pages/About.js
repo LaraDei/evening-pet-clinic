e8d9a9c44a2916fa5eb6b986804ef59d72c83e4e
Lara Samuelson <lara.samuelson@gmail.com>
2023-02-27 09:35:55 -0800

    created page structure

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+export default function About(){
+    return(
+        <div id='about' className='about'>
+            <p className='header'>
+                About
+            </p>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <div id='home' className='home'>
+            <p className='header'>
+                Evening Pet Clinic
+            </p>
+        </div>
+    )
+}
